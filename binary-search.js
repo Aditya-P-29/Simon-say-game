5bf1830eb834479815836d0866577d3dc3408dd0
Aditya-P-29 <adityapotdar7309@gmail.com>
2026-07-31 14:49:20 +0530

    made a change in mid of binarySearch algo to test ci

--- binary-search.js
+++ binary-search.js
@@ -2,12 +2,12 @@
 function binarySearch(arr, key) { 
     let start = 0, end = arr.length - 1; 
     while(start <= end) { 
-        let mid = Math.floor(start + (end - start) / 2); 
+        let mid = Math.floor(start + (end + start) / 2); 
         if(arr[mid] === key) return mid; 
         if(arr[mid] < key) { 
             start = mid + 1; 
         } else { 
-            end = mid + 1; 
+            end = mid - 1; 
         } 
     } 
     return -1; 
