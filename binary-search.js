d58da46dca047b53e870c59d88d5c7b3d32b24de
Aditya-P-29 <adityapotdar7309@gmail.com>
2026-07-31 14:52:34 +0530

    made a change in binarySearch algo condition to test ci

--- binary-search.js
+++ binary-search.js
@@ -2,8 +2,8 @@
 function binarySearch(arr, key) { 
     let start = 0, end = arr.length - 1; 
     while(start <= end) { 
-        let mid = Math.floor(start + (end + start) / 2); 
-        if(arr[mid] === key) return mid; 
+        let mid = Math.floor(start + (end - start) / 2); 
+        if(arr[mid] === key) return -1; 
         if(arr[mid] < key) { 
             start = mid + 1; 
         } else { 
