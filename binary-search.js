4088cc9d8c630e0a806919f711147787b2624fd6
Aditya-P-29 <adityapotdar7309@gmail.com>
2026-07-31 14:57:59 +0530

    fixed the logical error of binary-search.js

--- binary-search.js
+++ binary-search.js
@@ -3,7 +3,7 @@ function binarySearch(arr, key) {
     let start = 0, end = arr.length - 1; 
     while(start <= end) { 
         let mid = Math.floor(start + (end - start) / 2); 
-        if(arr[mid] === key) return -1; 
+        if(arr[mid] === key) return mid; 
         if(arr[mid] < key) { 
             start = mid + 1; 
         } else { 
